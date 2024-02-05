daec3dc6940b83442a44816adf9a328a684d85b4
binhong <544289495@qq.com>
2024-02-05 09:51:04 +0800

    feat: pwa settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,15 @@
 import { withContentlayer } from 'next-contentlayer';
-export default withContentlayer({});
+import withPWA from 'next-pwa';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const contentlayerConfig = withContentlayer({})
+const pwaConfig = withPWA({
+    dest:'public',
+    swSrc:'service-worker.js'
+})
+const nextConfig = {
+    ...contentlayerConfig,
+    ...pwaConfig,
+}
+
+export default nextConfig;
